c3fa021b18944d8ef4d3d019c2ba925c5d38d87b
zeinabsamir <zeinabsamir8@gmail.com>
2016-02-04 23:06:38 +0200

    Move creating tables into a separate sql file

--- db/seed.php
+++ db/seed.php
@@ -2,8 +2,8 @@
 require_once "database.php";
 
 // users
-$query = "INSERT INTO users (email, first_name, last_name, password, user_role, is_admin, created_at)
-          VALUES ('kathrin.meier@fci.zu.edu.eg', 'Kathrin', 'Meier', '12345678', 'student', '0', now())";
+$query = "INSERT INTO users (email, first_name, last_name, password, faculty, user_role, is_admin, created_at)
+          VALUES ('kathrin.meier@fci.zu.edu.eg', 'Kathrin', 'Meier', '12345678', 'Computer Science', 'student', '0', now())";
 
 if(mysql_query($query, $connection)) {
   echo "Student was created.\n";
---
+++ db/tables.sql
@@ -0,0 +1,107 @@
+CREATE TABLE users (
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        email VARCHAR(100) NOT NULL,
+                        first_name VARCHAR(100) NOT NULL,
+                        last_name VARCHAR(100) NOT NULL,
+                        password VARCHAR(24) NOT NULL,
+                        faculty VARCHAR(150) NOT NULL,
+                        date_of_birth DATETIME,
+                        user_role VARCHAR(30) NOT NULL DEFAULT 'student',
+                        is_admin TINYINT(1) DEFAULT 0,
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id),
+                        UNIQUE KEY email (email)
+                        );
+
+CREATE TABLE courses
+(
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        title VARCHAR(255),
+                        code VARCHAR(100),
+                        faculty VARCHAR(100) NOT NULL,
+                        department VARCHAR(100) NOT NULL,
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id),
+                        UNIQUE KEY code (code)
+                        );
+
+CREATE TABLE teaches (
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        course_id INT(11) NOT NULL,
+                        teacher_id INT(11) NOT NULL,
+                        year VARCHAR(100),
+                        semester VARCHAR(100),
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id)
+                        );
+
+CREATE TABLE studies (
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        course_id INT(11) NOT NULL,
+                        student_id INT(11) NOT NULL,
+                        year VARCHAR(100),
+                        semester VARCHAR(100),
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id)
+                        );
+
+CREATE TABLE attendance (
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        course_id INT(11) NOT NULL,
+                        student_id INT(11) NOT NULL,
+                        teacher_id INT(11) NOT NULL,
+                        has_attended TINYINT(1) DEFAULT 0,
+                        day DATETIME NOT NULL,
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id),
+                        UNIQUE KEY course_student_day (course_id, student_id, day)
+                        );
+
+CREATE TABLE announcements (
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        content TEXT NOT NULL,
+                        course_id INT(11) NOT NULL,
+                        teacher_id INT(11) NOT NULL,
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id)
+                        );
+
+CREATE TABLE grades (
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        content TEXT NOT NULL,
+                        course_id INT(11) NOT NULL,
+                        student_id INT(11) NOT NULL,
+                        teacher_id INT(11) NOT NULL,
+                        grade DECIMAL(7,2) NOT NULL DEFAULT 99999.99,
+                        type VARCHAR(150) NOT NULL,
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id)
+                        );
+
+CREATE TABLE student_gpas (
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        student_id INT(11) NOT NULL,
+                        GPA DECIMAL(7,2) NOT NULL DEFAULT 99999.99,
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id)
+                        );
+
+CREATE TABLE course_materials (
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        course_id INT(11) NOT NULL,
+                        teacher_id INT(11) NOT NULL,
+                        description TEXT,
+                        file_url VARCHAR(255),
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id)
+                        );
+
+CREATE TABLE course_assignments (
+                        id INT(11) NOT NULL AUTO_INCREMENT,
+                        course_id INT(11) NOT NULL,
+                        student_id INT(11) NOT NULL,
+                        file_url VARCHAR(255),
+                        type VARCHAR(150) NOT NULL,
+                        created_at DATETIME NOT NULL,
+                        PRIMARY KEY (id)
+                        );
