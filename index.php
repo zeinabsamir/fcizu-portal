26a9f5d8f41b76ddf9ae550f2bc9c0ff83ac5608
zeinabsamir <zeinabsamir8@gmail.com>
2016-02-05 00:20:16 +0200

    Setup MVC structure

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
-?>
+require_once("db/database.php");
 
-<?php include_once('partials/header.php'); ?>
-<?php include_once('partials/footer.php'); ?>
+if (isset($_GET['controller']) && isset($_GET['action'])) {
+  $controller = $_GET['controller'];
+  $action     = $_GET['action'];
+} else {
+  $controller = 'pages';
+  $action     = 'home';
+}
+
+require_once('views/layout.php');
+?>
