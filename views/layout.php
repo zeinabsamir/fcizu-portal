dab7428dedf415c4871b5baadfe4b0c33c4b6ae3
zeinabsamir <zeinabsamir8@gmail.com>
2016-02-06 13:10:05 +0200

    Add notice to layout

--- views/layout.php
+++ views/layout.php
@@ -14,6 +14,19 @@
 <?php require_once('views/partials/header.php'); ?>
 <!-- HEADER-->
 
+<!-- NOTICE -->
+<?php if(isset($_SESSION['notice'])) { ?>
+<div class="alert alert-info alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  <strong>Hey!</strong> <?= $_SESSION['notice'] ?>
+</div>
+<?php }
+
+unset($_SESSION['notice']);
+
+?>
+<!-- NOTICE -->
+
 <div class="container">
   <div class="row">
     <!-- SIDEBAR -->
