d1b8bcb6f20a48a445ef728827ad0b74c0b7e2f7
zeinabsamir <zeinabsamir8@gmail.com>
2016-02-29 17:41:16 +0200

    Add courses link to the sidebar

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -1,5 +1,5 @@
 <ul class="nav nav-stacked" id="sidebar">
-  <li><a href="#sec0">Section 0</a></li>
+  <li><a href="?controller=courses&action=index">Courses</a></li>
   <li><a href="#sec1">Section 1</a></li>
   <li><a href="#sec2">Section 2</a></li>
   <li><a href="#sec3">Section 3</a></li>
