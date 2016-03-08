031a2d7dc2636c3dcbe84e3d4982e59db76defd4
zeinabsamir <zeinabsamir8@gmail.com>
2016-03-08 09:29:03 +0200

    Add tables for listing users and courses

--- views/courses/index.php
+++ views/courses/index.php
@@ -1,11 +1,27 @@
 <h1>Listing Courses</h1>
 
-<?php foreach($courses as $course) { ?>
-  <p>
-    <?php echo $course->title; ?>
-    <a href='?controller=courses&action=show&id=<?php echo $course->id; ?>'>Show</a>
-    <a href="?controller=courses&action=edit&id=<?php echo $course->id ?>">Edit</a>
-    <a href='?controller=courses&action=destroy&id=<?php echo $course->id; ?>'>Delete</a>
-  </p>
-<?php } ?>
+<table class="table">
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>Title</th>
+      <th>Code</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
 
+  <tbody>
+    <?php foreach($courses as $course) { ?>
+      <tr>
+        <td><?php echo $course->id; ?></td>
+        <td><?php echo $course->title; ?></td>
+        <td><?php echo $course->code; ?></td>
+        <td>
+          <a href='?controller=courses&action=show&id=<?php echo $course->id; ?>'>Show</a> |
+          <a href="?controller=courses&action=edit&id=<?php echo $course->id ?>">Edit</a> |
+          <a href='?controller=courses&action=destroy&id=<?php echo $course->id; ?>'>Delete</a>
+        </td>
+      </tr>
+    <?php } ?>
+  </tbody>
+</table>
