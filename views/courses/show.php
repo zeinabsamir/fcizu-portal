837e44903eb199cbcb3bebea32dc6a24d4c613fe
zeinabsamir <zeinabsamir8@gmail.com>
2016-03-08 09:54:05 +0200

    Add stlying for course show page

--- views/courses/show.php
+++ views/courses/show.php
@@ -1,4 +1,38 @@
-<h1>Show Course</h1>
-
-<p><?php echo $course->title; ?></p>
-<p><?php echo $course->code; ?></p>
+<div class="col-lg-3 col-md-4 col-sm-5 col-lg-offset-2 col-md-offset-2 col-sm-offset-2 text-center">
+  <div class="form-box">
+    <div class="row ">
+      <div class="col-lg-12">
+        <form>
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="form-element">
+                <h3 class="text-center"><?php echo $course->title; ?></h3>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="form-element form-group">
+                Code <h4><?php echo $course->code; ?></h4>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="form-element form-group">
+                Faculty <h4><?php echo $course->faculty; ?></h4>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="form-element form-group">
+                Department <h4><?php echo $course->department; ?></h4>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
