b1faeb0480b9dceecd84458eb06ebbc662da50d2
zeinabsamir <zeinabsamir8@gmail.com>
2016-06-21 00:40:00 +0200

    Use select tags for courses listing pages

--- views/announcements/index.php
+++ views/announcements/index.php
@@ -1,4 +1,20 @@
 <h1>Announcements</h1>
-<?php foreach($myCoursesList as $course) { ?>
-  <a href='?controller=announcements&action=show&course_id=<?php echo $course->id; ?>'><?= $course->title ?></a>
-<?php } ?>
+
+<br>
+
+<div class="form-element form-group">
+  <select name="courses" id="courses" class="form-control">
+    <?php foreach($myCoursesList as $course) { ?>
+      <option value="?controller=announcements&action=show&course_id=<?php echo $course->id; ?>"><?= $course->title ?></option>
+    <?php } ?>
+  </select>
+</div>
+
+<a id="goToPage" onClick="changePath()" href="" class="btn btn-default">View Page</a>
+
+<script type="text/javascript">
+function changePath() {
+  var path= document.getElementById("courses").value;
+  $("#goToPage").attr('href', path);
+}
+</script>
--- views/materials/index.php
+++ views/materials/index.php
@@ -1,5 +1,20 @@
 <h1>Materials</h1>
-<?php foreach($myCoursesList as $course) { ?>
-  <a href='?controller=materials&action=show&course_id=<?php echo $course->id; ?>'><?= $course->title ?></a>
-<?php } ?>
 
+<br>
+
+<div class="form-element form-group">
+  <select name="courses" id="courses" class="form-control">
+    <?php foreach($myCoursesList as $course) { ?>
+      <option value="?controller=materials&action=show&course_id=<?php echo $course->id; ?>"><?= $course->title ?></option>
+    <?php } ?>
+  </select>
+</div>
+
+<a id="goToPage" onClick="changePath()" href="" class="btn btn-default">View Page</a>
+
+<script type="text/javascript">
+function changePath() {
+  var path= document.getElementById("courses").value;
+  $("#goToPage").attr('href', path);
+}
+</script>
